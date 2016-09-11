1af6369f5a4cbac2bed8b40354ff2bb8f3a6d633
Milen Stanev <milen.stanev@gmail.com>
2016-09-11 19:09:54 +0300

    Remove lib, add .sh files

---
+++ get-lib-template.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# TODO: To be described
+
+cd ./lib
+git clone https://github.com/milenstanev/msw.component.template.git
---
+++ get-lib.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# TODO: To be described
+
+cd ./lib
+#
+git clone https://github.com/milenstanev/msw.component.template.git
+#
+git clone https://github.com/milenstanev/msw.core.git
